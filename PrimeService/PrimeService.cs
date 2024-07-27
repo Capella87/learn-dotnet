140e0b1e1fc4684181398ce01ccaccd0057b9bf5
Capella87 <capella87@outlook.com>
2024-07-27 12:10:31 +0900

    Implement PrimeService

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -2,8 +2,31 @@ namespace Prime.Services;
 
 public class PrimeService
 {
+    public PrimeService()
+    {
+
+    }
+
     public bool IsPrime(int candidate)
     {
-        throw new NotImplementedException("Not implemented");
+        if (candidate < 2)
+        {
+            return false;
+        }
+
+        return EratosthenesSieve(candidate);
+    }
+
+    private bool EratosthenesSieve(int target)
+    {
+        var boundary = Math.Sqrt(target);
+        for (int divisor = 2; divisor <= boundary; divisor++)
+        {
+            if (target % divisor == 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
